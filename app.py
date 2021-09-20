d11e869754d3805f19b98b6d014e9aad64284ee0
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2021-09-20 15:54:33 -0300

    Implementing my changes pt1

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ from time import sleep
 
 class TinderBot:
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path='chromedriver.exe')
-        self.driver.get('https://www.tinder.com')
+        self.driver = webdriver.Chrome(executable_path='chromedriver.exe')     #open chrome browser
+        self.driver.get('https://www.tinder.com')                              #go to url
         sleep(3)
 
-    def login(self):
-        botao_login = self.driver.find_element_by_xpath(
-            '//*[@id="c2094796203"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+    def login(self):                                                           #find login_button in the web page
+        login_button = self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
         sleep(3)
-        botao_login.click()
-        sleep(3)
-
-    def logar_com_telefone(self):
-        logar_com_telefone = self.driver.find_element_by_xpath(
-            '//*[@id="c366415127"]/div/div/div[1]/div/div[3]/span/div[3]/button')
-        sleep(3)
-        logar_com_telefone.click()
+        login_button.click()
         sleep(3)
 
-    def esperar_input_manual(self):
+    def login_method(self):                                                     #choose the phone number to login
+        enter_button = self.driver.find_element_by_xpath(
+        '//*[@id="o-1268705039"]/div/div/div[1]/div/div[3]/span/div[3]/button')
+        sleep(1)
+        enter_button.click()
+        sleep(1)
+
+    def waiting_for_manual_input(self):
         input()
 
     def fechar_notificao_de_localizacao(self):
@@ -70,8 +70,8 @@ class TinderBot:
 
 bot = TinderBot()
 bot.login()
-bot.logar_com_telefone()
-bot.esperar_input_manual()
+bot.login_method()
+bot.waiting_for_manual_input()
 bot.fechar_notificao_de_localizacao()
 bot.fechar_notificao_de_notificacoes()
 
